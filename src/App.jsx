81d5202999f19a5db57c1137f7eafe33eab018b0
Omar Mattos <omarmatt84@gmail.com>
2025-06-25 16:05:29 -0300

    se agrego el arreglo de peliculas, la función para agregar peliculas, y el maquetado del formulario con su correspondiente validación de campos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import FormularioPeliculas from "./components/FormularioPeliculas";
 
 function App() {
 
   return (
     <>
       <main>
-        <h1>Hola Mundo</h1>
+        <h1 className="text-center my-4">Alta de Peliculas</h1>
+        <FormularioPeliculas></FormularioPeliculas>
       </main>
       <footer className="bg-dark text-light text-center py-3">
         <p>💻 Omar Mattos 💻</p>
---
+++ src/components/FormularioPeliculas.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form"
+
+const FormularioPeliculas = () => {
+    const [peliculas, setPeliculas]=useState([])
+
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm()
+
+    const agregarPelicula= (data)=>{
+        setPeliculas([...peliculas, data])
+        reset()
+    }
+
+    return (
+        <>
+            <section className="container bg-secondary py-4 px-5 rounded-3 w-50 mb-3">
+                <Form onSubmit={handleSubmit(agregarPelicula)}>
+                        <Form.Group className="mb-2" controlId="formBasicNombre">
+                            <Form.Label className="text-light">Nombre de la Pelicula</Form.Label>
+                            <Form.Control type="text" placeholder="Ej: El Señor de los Anillos" {...register('inputNombre', {
+                                required: 'Ingrese el nombre de la Pelicula'
+                            })}/>
+                            <Form.Text className="mb-2 text-danger">{errors.inputNombre?.message}</Form.Text>
+                        </Form.Group>
+                        
+                        <Form.Group className="mb-3" controlId="formBasicDuenio">
+                            <Form.Label className="text-light">Descripción</Form.Label>
+                            <Form.Control as="textarea" {...register('inputDescripcion', {
+                                required: 'Ingrese la descripción de la pelicula'
+                            })}/>
+                            <Form.Text className="mb-2 text-danger">{errors.inputDescripcion?.message}</Form.Text>
+                        </Form.Group>
+                        <Form.Group className="mb-5 w-50" controlId="formBasicSintomas">
+                            <Form.Label className="text-light">Género</Form.Label>
+                            <Form.Select {...register('inputGenero', {
+                                required: 'Seleccione un genero'
+                            })}>
+                                <option value="" selected disabled hidden>Seleccione un genero</option>
+                                <option>Comedia</option>
+                                <option>Drama</option>
+                                <option>Infantil</option>
+                            </Form.Select>
+                            <Form.Text className="mb-2 text-danger">{errors.inputGenero?.message}</Form.Text>
+                        </Form.Group>
+                    <div className="text-center">
+                        <Button variant="info" type="submit" className="mt-3 text-light">
+                            Guardar Pelicula
+                        </Button>
+                    </div>
+                </Form>
+            </section>
+        </>
+    );
+};
+
+export default FormularioPeliculas;
